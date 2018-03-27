981a3c30dc247590df2798dd39e33fb9e2085931
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-27 03:28:23 +0000

    Made getter to link city and state classes

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
     Implementation of the State class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class State(BaseModel, Base):
@@ -13,3 +13,15 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     __tablename__ = "states"
     cities = relationship("City", backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+        '''
+            Getter attribute that returns a list of cities
+        '''
+        city_list = []
+        dicti = storage.all(City)
+        for key, value in dicti:
+            if value.state_id == self.id
+                city_list.append(value)
+        return city_list
