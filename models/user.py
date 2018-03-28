455194e34071b9bbe0435f32f8702980cc559a5d
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-03-28 21:29:12 +0000

    Connected 'users' class to database with sqlalchemy and created attributes for it

--- models/user.py
+++ models/user.py
@@ -2,14 +2,20 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
