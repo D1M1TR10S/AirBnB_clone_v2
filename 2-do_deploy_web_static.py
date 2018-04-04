3ac578a32cae33ddca5e29221075c728e8066a50
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-04 02:41:10 +0000

    Made #2 pep8 compliant

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,8 +8,8 @@ from fabric.api import *
 from time import strftime
 import os
 
-
 env.hosts = ['107.23.115.192', '34.228.158.73']
+
 
 def do_deploy(archive_path):
     '''
@@ -22,13 +22,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         new_path = "/tmp/{}.tgz".format(archive)
         run("mkdir -p /data/web_static/releases/{}/".format(archive))
-        run("tar -xzf {} -C /data/web_static/releases/{}/".format(new_path, archive))
+        run("tar -xzf {} -C /data/web_static/releases/{}/"
+            .format(new_path, archive))
         run("rm /tmp/{}.tgz".format(archive))
         run("mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(archive, archive))
         run("rm -rf /data/web_static/releases/{}/web_static/".format(archive))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive))
         return True
 
     except:
