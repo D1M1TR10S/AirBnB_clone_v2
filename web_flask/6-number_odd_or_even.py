0870d720aa8c0862d08d6054c2a02a148fef89af
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-19 00:39:14 +0000

    Changed integer checker

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -33,25 +33,23 @@ def python_is_cool(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
     '''Displays 'n is a number' if it is an int'''
-    if n.isdigit():
-        return '{} is a number'.format(n)
+    return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def num_template(n):
     '''Displays an HTML page if n is an int'''
-    if n.isdigit():
-        return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_even(n):
     '''Renders an HTML page with different output for odd and even numbers'''
-    if n.isdigit():
-        return render_template('6-number_odd_or_even.html', n=n)
+    return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
