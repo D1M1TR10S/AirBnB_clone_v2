fc37b048a15a5e1826e3baf42fbd3e31a1d79fcd
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-04-19 00:26:57 +0000

    Made python-flask scripts pep8 compliant

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -14,15 +14,18 @@ def hello_hbnb():
     '''Routes 'Hello HBNB!' to index'''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''Routes 'HBNB!' to /hbnb'''
     return 'HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     '''Returns 'C ' + user input'''
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -30,11 +33,13 @@ def python_is_cool(text='is cool'):
     '''Returns 'Python ' + user input & defaults to (Python is cool)'''
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<n>', strict_slashes=False)
 def is_number(n):
     '''Displays 'n is a number' if it is an int'''
     if n.isdigit():
         return '{} is a number'.format(n)
+
 
 @app.route('/number_template/<n>', strict_slashes=False)
 def num_template(n):
@@ -42,5 +47,5 @@ def num_template(n):
     if n.isdigit():
         return render_template('5-number.html', n=n)
 
-if __name__=='__main__':
-    app.run(host= '0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -13,15 +13,18 @@ def hello_hbnb():
     '''Routes 'Hello HBNB!' to index'''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''Routes 'HBNB!' to /hbnb'''
     return 'HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     '''Returns 'C ' + user input'''
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,11 +32,13 @@ def python_is_cool(text='is cool'):
     '''Returns 'Python ' + user input & defaults to (Python is cool)'''
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<n>', strict_slashes=False)
 def is_number(n):
     '''Displays 'n is a number' if it is an int'''
     if n.isdigit():
         return '{} is a number'.format(n)
+
 
 @app.route('/number_template/<n>', strict_slashes=False)
 def num_template(n):
@@ -41,11 +46,12 @@ def num_template(n):
     if n.isdigit():
         return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<n>', strict_slashes=False)
 def odd_even(n):
     '''Renders an HTML page with different output for odd and even numbers'''
     if n.isdigit():
         return render_template('6-number_odd_or_even.html', n=n)
 
-if __name__=='__main__':
-    app.run(host= '0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
